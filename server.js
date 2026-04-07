242ab06b1e56ce8900cba3310da0a167404dbe7b
ifdktskfkgkgktktkt-blip <ifdktskfkgkgktktkt@gmail.com>
2026-04-08 00:06:30 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname)));
 
 const PORT = process.env.PORT || 3000;
 
-http.createServer((req, res) => {
-  fs.readFile("index.html", (err, data) => {
-    if (err) {
-      res.writeHead(500);
-      res.end("Error loading file");
-      return;
-    }
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(data);
-  });
-}).listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
 });
